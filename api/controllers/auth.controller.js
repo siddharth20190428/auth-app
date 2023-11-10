2655f12cab91db0dd1fe7b67b68e7607efe71a6f
Siddharth <siddharth201820@gmail.com>
2023-11-11 02:06:43 +0530

    sign in fix

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -20,12 +20,12 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      next(errorHandler(404, "User Not Found"));
+      return next(errorHandler(404, "User Not Found"));
     }
 
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-      next(errorHandler(401, "Invalid Credentials"));
+      return next(errorHandler(401, "Invalid Credentials"));
     }
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
