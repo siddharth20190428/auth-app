d0eaa5be4215160c6239a1230d2760f02037a712
Siddharth <siddharth201820@gmail.com>
2023-11-12 00:30:36 +0530

    completed home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About</h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut distinctio,
+        nesciunt unde aut earum nulla, deleniti mollitia officia placeat, magnam
+        hic omnis praesentium fugit! Veniam saepe, perspiciatis incidunt,
+        repudiandae reiciendis, fugit ullam corrupti quis sequi cum esse maiores
+        tempora. Explicabo veniam eum dolorum vel perferendis nulla a tempore
+        quidem libero!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut distinctio,
+        nesciunt unde aut earum nulla, deleniti mollitia officia placeat, magnam
+        hic omnis praesentium fugit! Veniam saepe, perspiciatis incidunt,
+        repudiandae reiciendis, fugit ullam corrupti quis sequi cum esse maiores
+        tempora. Explicabo veniam eum dolorum vel perferendis nulla a tempore
+        quidem libero!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut distinctio,
+        nesciunt unde aut earum nulla, deleniti mollitia officia placeat, magnam
+        hic omnis praesentium fugit! Veniam saepe, perspiciatis incidunt,
+        repudiandae reiciendis, fugit ullam corrupti quis sequi cum esse maiores
+        tempora. Explicabo veniam eum dolorum vel perferendis nulla a tempore
+        quidem libero!
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        Welcome to the Authentication tab
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut distinctio,
+        nesciunt unde aut earum nulla, deleniti mollitia officia placeat, magnam
+        hic omnis praesentium fugit! Veniam saepe, perspiciatis incidunt,
+        repudiandae reiciendis, fugit ullam corrupti quis sequi cum esse maiores
+        tempora. Explicabo veniam eum dolorum vel perferendis nulla a tempore
+        quidem libero!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut distinctio,
+        nesciunt unde aut earum nulla, deleniti mollitia officia placeat, magnam
+        hic omnis praesentium fugit! Veniam saepe, perspiciatis incidunt,
+        repudiandae reiciendis, fugit ullam corrupti quis sequi cum esse maiores
+        tempora. Explicabo veniam eum dolorum vel perferendis nulla a tempore
+        quidem libero!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut distinctio,
+        nesciunt unde aut earum nulla, deleniti mollitia officia placeat, magnam
+        hic omnis praesentium fugit! Veniam saepe, perspiciatis incidunt,
+        repudiandae reiciendis, fugit ullam corrupti quis sequi cum esse maiores
+        tempora. Explicabo veniam eum dolorum vel perferendis nulla a tempore
+        quidem libero!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
