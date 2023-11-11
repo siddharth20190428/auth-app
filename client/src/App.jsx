66b113d29b007be48e81d1a39e7f0870f14bc345
Siddharth <siddharth201820@gmail.com>
2023-11-11 17:52:31 +0530

    made profile private and updated header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-300">
       <div className="flex justify-between p-4 max-w-6xl mx-auto items-center">
         <Link to="/">
           <h1 className="font-bold text-2xl">Auth</h1>
         </Link>
-        <ul className="flex gap-4">
+        <ul className="flex gap-4 items-center">
           <Link to="/">
             <li>Home</li>
           </Link>
           <Link to="/about">
             <li>About</li>
           </Link>
-          <Link to="/signin">
-            <li>SignIn</li>
-          </Link>
+          {currentUser ? (
+            <Link to="/profile">
+              <img
+                src={currentUser.profilePicture}
+                alt="Profile"
+                className="h-8 w-8 object-cover rounded-full"
+              />
+            </Link>
+          ) : (
+            <Link to="/signin">
+              <li>SignIn</li>
+            </Link>
+          )}
         </ul>
       </div>
     </div>
